5a570dfbceba16473ca94c6b86ce419e717fa0f3
deris <305945915@qq.com>
2018-06-19 14:56:30 +0800

    add remove methods

--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,6 @@
 export default class EventBus {
-    bus: any;
-    private uid: number;
-
-    constructor() {
-        this.bus = {}
-    }
+    bus: any = {};
+    uid: number = 1;
 
     public on(eventName: string, cb: () => any) {
         let ei = new EventItem(cb, this.uid)
@@ -16,12 +12,30 @@ export default class EventBus {
         }
 
         this.uid++
+
+        return ei
     }
 
-    public fire(eventName: string, args: any) {
+    public fire(eventName: string, args: any = undefined) {
         let targets = this.bus[eventName]
+
+        if (!targets) return
+
         for (let idx in targets) {
             targets[idx].fucker(args)
+        }
+    }
+
+    public remove(eventName: string, eventItem: EventItem) {
+        if (!this.bus[eventName]) return
+
+        let targets = this.bus[eventName]
+
+        for (let idx in targets) {
+            if (targets[idx] === eventItem) {
+                targets.splice(idx, 1)
+                break;
+            }
         }
     }
 }
